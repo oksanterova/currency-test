92a7359ab15ea7655fb37a64faef249b8a3389f4
oksanterova <oksana.kanterova@gmail.com>
2019-12-04 01:38:23 +0100

    Fetch fixer data and add fake-fixer due to limitations

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const { ApolloServer, gql } = require("apollo-server-express");
+const fakeFixer = require("./fake_fixer");
 
 let config = {
+  accessKey: process.env.ACCESS_KEY,
+  fakeFixer: process.env.FAKE_FIXER === "true",
   port: process.env.PORT || 4000
 };
 
@@ -38,14 +42,46 @@ const typeDefs = gql`
     symbol: String!
   }
 
+  type Rate {
+    symbol: String!
+    rate: Float!
+  }
+
   type Query {
     currencies: [Currency!]!
+    rates(base: String!): [Rate!]!
   }
 `;
 
+const fetchLatest = async (base, symbols) => {
+  if (config.fakeFixer) {
+    return fakeFixer[base];
+  }
+
+  const url = `http://data.fixer.io/api/latest?access_key=${
+    config.accessKey
+  }&base=${base}&symbols=${symbols.join(",")}`;
+
+  const res = await fetch(url);
+  return await res.json();
+};
+
 const resolvers = {
   Query: {
-    currencies: () => currencies
+    currencies: () => currencies,
+    rates: async (_, { base }) => {
+      const symbols = currencies.map(({ symbol }) => symbol);
+      const data = await fetchLatest(base, symbols);
+
+      if (!data.success) {
+        throw new Error(data.error.type);
+      }
+
+      return Object.entries(data.rates).map(([symbol, rate]) => ({
+        symbol,
+        rate
+      }));
+    }
   }
 };
 
